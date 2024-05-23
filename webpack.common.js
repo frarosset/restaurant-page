7f2b642709388640d7fbb90cfd612982d177db6f
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-23 17:42:06 +0200

    Install html-webpack-plug and configure webpack for it
    The configuration include:
    1) configuring the html template to use through plugin settings in
       webpack.common.js.
    2) add .html files in ./src folder to the files watched by dev
       server in development mode.

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
    entry: {
       main: './src/index.js',
    },
+   plugins: [
+    new HtmlWebpackPlugin({
+       title: 'Restaurant page',
+       template: './src/index.html',
+    }),
+   ],
    output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,5 +6,6 @@ module.exports = merge( common, {
    devtool: 'inline-source-map',
    devServer: {
       static: './dist',
+      watchFiles: ['src/**/*.html'],
    },
 });
