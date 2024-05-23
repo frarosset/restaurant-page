f3be47e91529742e0aabd0133ebac2e191e55f4b
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-23 18:15:52 +0200

    Configure webpack for handling images and fonts
    Images can be imported as follows:
    1) import MyImage from './my-image.png' [IN .JS]
       (MyImage variable contains its final url)
    2) url('./my-image.png') [in .CSS]
       (the url is replaced when also using the css-loader)
    3) <img src="./my-image.png" /> [in .HTML]
       (the url is replaced when also using the html-loader)
    
    Fonts can be imported as follows:
    1) [IN .CSS]
       @font-face {
          font-family: 'MyFont';
          src: url('./my-font.woff2') format('woff2'),
               url('./my-font.woff') format('woff');
          font-weight: 600;
          font-style: normal;
       }
    
    In all cases, the image/font is processed and added to the output directory.

--- webpack.common.js
+++ webpack.common.js
@@ -2,26 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: {
-      main: './src/index.js',
-   },
-   plugins: [
-    new HtmlWebpackPlugin({
-       title: 'Restaurant page',
-       template: './src/index.html',
-    }),
-   ],
-   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+    entry: {
+        main: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant page',
+            template: './src/index.html',
+        }),
     ],
-  },
-   output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-   },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
 };
