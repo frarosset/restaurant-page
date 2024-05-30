41e075ad4a80fd817cec161dd63c8aa7b120c0b2
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-30 14:59:59 +0200

    Add callback to the logo to return to the home page

--- src/initWebpage.js
+++ src/initWebpage.js
@@ -5,6 +5,10 @@ export default function initWebpage(){
     const contentDiv = document.querySelector('main#content');
     renderHome(contentDiv);
 
+    const logoAndName = document.querySelector('.logo-and-name');
+    logoAndName.contentDiv = contentDiv;
+    logoAndName.addEventListener('click',(e) => {renderHome(e.currentTarget.contentDiv)});
+
     const aboutBtn = document.querySelector('.about-btn');
     aboutBtn.contentDiv = contentDiv;
     aboutBtn.addEventListener('click',(e) => {renderAbout(e.currentTarget.contentDiv)});
